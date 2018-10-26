c5333016a2ea8eaa9742970ccc8643c9f833ec1c
Pedro Vega <pakgva@gmail.com>
2018-10-26 17:40:30 -0500

    update docker.mk :recycle: 14

--- makefiles/docker.mk
+++ makefiles/docker.mk
@@ -1,8 +1,8 @@
 .PHONY: docker
 
-VOLUMEN_DOCKER = volumen_docker_f
-NETWORK_NAME = red_docker_f
-JENKINS_NAME = jenkins_name_f
+VOLUMEN_DOCKER = volumen_docker_final
+NETWORK_NAME = red_docker_final
+JENKINS_NAME = jenkins_name_final
 
 docker-workspace:
 	echo "create docker worspace"
@@ -16,7 +16,7 @@ docker-install:
 docker-start:
 	#docker network rm ${NETWORK_NAME}
 	docker network create ${NETWORK_NAME}
-	docker rm -f ${JENKINS_NAME}
+	#docker rm -f ${JENKINS_NAME}
 	docker run -d --net=${NETWORK_NAME} --name ${JENKINS_NAME} \
 	-p 3030:1042 --volumes-from ${VOLUMEN_DOCKER} -w /app ${DOCKER_IMAGE} npm start
 
