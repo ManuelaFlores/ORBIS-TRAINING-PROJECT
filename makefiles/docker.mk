a65e9d913a028dac408220ea928289c507a56030
Pedro Vega <pakgva@gmail.com>
2018-10-26 17:12:05 -0500

    update docker.mk :recycle: 5

--- makefiles/docker.mk
+++ makefiles/docker.mk
@@ -5,13 +5,15 @@ NETWORK_NAME = red_docker
 
 docker-workspace:
 	echo "create docker worspace"
-	#docker create -v /app --name ${VOLUMEN_DOCKER} alpine
+	docker rm ${VOLUMEN_DOCKER}
+	docker create -v /app --name ${VOLUMEN_DOCKER} alpine
 	docker cp ./ ${VOLUMEN_DOCKER}:/app
 
 docker-install:
 	docker run -t --rm --volumes-from ${VOLUMEN_DOCKER} -w /app ${DOCKER_IMAGE} npm install
 
 docker-start:
+	docker network rm ${NETWORK_NAME}
 	docker network create ${NETWORK_NAME}
 	docker run -t  --rm --net=${NETWORK_NAME} --name jenkins_name \
 	-p "3030:1042" --volumes-from ${VOLUMEN_DOCKER} -w /app ${DOCKER_IMAGE} npm start
