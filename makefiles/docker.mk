3b551957349301ae65f5644ff69bea278933c82c
Pedro Vega <pakgva@gmail.com>
2018-10-26 16:53:55 -0500

    update docker.mk :recycle: 2

--- makefiles/docker.mk
+++ makefiles/docker.mk
@@ -4,7 +4,7 @@ VOLUMEN_DOCKER = volumen_docker
 
 docker-workspace:
 	echo "create docker worspace"
-	docker create -v /app --name ${VOLUMEN_DOCKER} alpine
+	#docker create -v /app --name ${VOLUMEN_DOCKER} alpine
 	docker cp ./ ${VOLUMEN_DOCKER}:/app
 
 docker-install:
